c52272cac2758118c8cfc70c18dda5e7061f1bc3
Savini Himasara <himasara210@gmail.com>
2023-07-08 02:21:18 +0530

    database functions changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { TeacherTableComponent } from './components/teacher-table/teacher-table.
 
 const routes: Routes = [
   { path: '', component: TeacherTableComponent },
-  { path: '', component: StudentTableComponent },
+  { path: 'student', component: StudentTableComponent },
   { path: 'addTeacher', component: AddNewTeacherComponent },
   { path: 'addStudent', component: AddNewStudentComponent },
   { path: 'editStudent', component: EditStudentComponent },
